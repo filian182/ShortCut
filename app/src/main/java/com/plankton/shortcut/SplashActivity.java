fd76cadb5ea74a0b56fc861a2e6cfb52f1665887
filian182 <filiananjasmara182@gmail.com>
2022-08-26 09:02:22 +0700

    Update 26/08/2022 Responsive Layout

---
+++ app/src/main/java/com/plankton/shortcut/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.plankton.shortcut;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread mythread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        mythread.start();
+    }
+}
